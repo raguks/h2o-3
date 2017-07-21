2c245f6f5c5c02640e61185212a7d650462ef58e
michalkurka <michalk@h2o.ai>
2017-07-21 09:20:59 -0700

    Clean-up and clarification of code related to processing non-UTF-8 strings

--- h2o-parsers/h2o-parquet-parser/src/test/java/water/parser/parquet/ParseTestParquet.java
+++ h2o-parsers/h2o-parquet-parser/src/test/java/water/parser/parquet/ParseTestParquet.java
@@ -30,7 +30,6 @@ import static org.junit.Assert.*;
 
 import water.TestUtil;
 import water.fvec.Frame;
-import water.fvec.RollupStatsHelpers;
 import water.fvec.Vec;
 import water.parser.BufferedString;
 
@@ -99,7 +98,7 @@ public class ParseTestParquet extends TestUtil {
           assertEquals("Value in column mylong", 2 + row, f.vec(2).at8(row));
           assertEquals("Value in column myfloat", 3.1f + row, f.vec(3).at(row), EPSILON);
           assertEquals("Value in column myfloat", 4.1 + row, f.vec(4).at(row), EPSILON);
-          assertEquals("Value in column mystring", "hello world: " + row, f.vec(7).atStr(bs, row).bytesToString());
+          assertEquals("Value in column mystring", "hello world: " + row, f.vec(7).atStr(bs, row).toSanitizedString());
           assertEquals("Value in column myenum", row % 2 == 0 ? "a" : "b", f.vec(8).factor(f.vec(8).at8(row)));
         }
       }
